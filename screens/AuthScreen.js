b18b277565922fb0c7f72d9d955f7d63869153b7
Kayla <keoliva@andrew.cmu.edu>
2019-01-18 18:25:52 -0500

    including secrets file in .gitignore file

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import {} from 'react-native';
 import LoginButton from '../components/SpotifyLoginButton';
+import { AuthSession } from 'expo';
+import secret from 'secrets';
+
+const REDIRECT_URL = AuthSession.getRedirectUrl();
+const spotify = {
+	id: '9ba6f7d5f13a4441b16cefba57cbdef6',
+	secret,
+};
 
 export default class AuthScreen extends React.Component {
+	async _signInAsync() {}
 	render() {
-		return <LoginButton onPress />;
+		return <LoginButton onPress={this._signInAsync} />;
 	}
 }
